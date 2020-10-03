ea50eef605a31816da3eef325296fee4a43732ce
Paul Davis <paul@linuxaudiosystems.com>
2020-10-02 21:26:17 -0600

    another day of slow timeline type conversion

--- gtk2_ardour/route_time_axis.h
+++ gtk2_ardour/route_time_axis.h
@@ -100,7 +100,7 @@ public:
 	void selection_click (GdkEventButton*);
 	void set_selected_points (PointSelection&);
 	void set_selected_regionviews (RegionSelection&);
-	void get_selectables (ARDOUR::samplepos_t start, ARDOUR::samplepos_t end, double top, double bot, std::list<Selectable *>&, bool within = false);
+	void get_selectables (Temporal::timepos_t const &, Temporal::timepos_t const &, double top, double bot, std::list<Selectable *>&, bool within = false);
 	void get_inverted_selectables (Selection&, std::list<Selectable*>&);
 	void set_layer_display (LayerDisplay d);
 	void toggle_layer_display ();
