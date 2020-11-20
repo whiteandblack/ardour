85f3282f599247e5db6af4aca7ac4568440f53ef
Robin Gareus <robin@gareus.org>
2020-11-20 18:29:42 +0100

    Fix missing Lua Binding

--- libs/ardour/luabindings.cc
+++ libs/ardour/luabindings.cc
@@ -1700,6 +1700,9 @@ LuaBindings::common (lua_State* L)
 		.beginStdList <boost::shared_ptr<AudioTrack> > ("AudioTrackList")
 		.endClass ()
 
+		.beginStdList <TimelineRange> ("TimelineRangeList")
+		.endClass ()
+
 		// std::list<boost::shared_ptr <MidiTrack> >
 		.beginStdList <boost::shared_ptr<MidiTrack> > ("MidiTrackList")
 		.endClass ()
