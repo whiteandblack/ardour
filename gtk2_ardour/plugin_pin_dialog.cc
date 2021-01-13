d2eed83b71fb0f92f2ffd8b4e5680e4fd904a7c9
Paul Davis <paul@linuxaudiosystems.com>
2021-01-13 09:02:59 -0700

    Automatable now requires (and owns) a time domain to be used by automation data (GUI edition)

--- gtk2_ardour/plugin_pin_dialog.cc
+++ gtk2_ardour/plugin_pin_dialog.cc
@@ -1743,7 +1743,7 @@ PluginPinWidget::add_send_from (boost::weak_ptr<ARDOUR::Port> wp, boost::weak_pt
 		return;
 	}
 
-	boost::shared_ptr<Pannable> sendpan (new Pannable (*_session));
+	boost::shared_ptr<Pannable> sendpan (new Pannable (*_session, Config->get_default_automation_time_domain()));
 	boost::shared_ptr<Send> send (new Send (*_session, r->pannable (), r->mute_master ()));
 	const ChanCount& outs (r->amp ()->input_streams ());
 	try {
