f693e76a55ab46363514edd9a1aa91f6d50a24f7
Paul Davis <paul@linuxaudiosystems.com>
2021-01-22 13:41:15 -0700

    fix types for MIDI source API, and remove unusued loop length member

--- libs/ardour/disk_writer.cc
+++ libs/ardour/disk_writer.cc
@@ -345,8 +345,16 @@ void
 DiskWriter::non_realtime_locate (samplepos_t position)
 {
 	if (_midi_write_source) {
-#warning NUTEMPO maybe fixme perhaps take sources time domain into account here e.g. beats
-		_midi_write_source->set_natural_position (timepos_t (position));
+		timepos_t pos;
+
+		if (time_domain() == Temporal::AudioTime) {
+			pos = timepos_t (position);
+		} else {
+			const timepos_t b (position);
+			pos = timepos_t (b.beats());
+		}
+
+		_midi_write_source->set_natural_position (pos);
 	}
 
 	DiskIOProcessor::non_realtime_locate (position);
@@ -469,7 +477,16 @@ DiskWriter::run (BufferSet& bufs, samplepos_t start_sample, samplepos_t end_samp
 			}
 
 			if (_midi_write_source) {
-				_midi_write_source->mark_write_starting_now (_capture_start_sample, _capture_captured, loop_length.samples());
+
+				timepos_t start;
+
+				start = timepos_t (_capture_start_sample);
+
+				if (time_domain() != Temporal::AudioTime) {
+					start = timepos_t (start.beats());
+				}
+
+				_midi_write_source->mark_write_starting_now (start, _capture_captured);
 			}
 
 			g_atomic_int_set (const_cast<gint*> (&_samples_pending_write), 0);
